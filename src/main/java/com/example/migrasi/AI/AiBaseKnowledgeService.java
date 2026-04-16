b7d419486aa3f344a63495efac341adf013a0e40
fajri <raden.alfajri@metrocom.co.id>
2026-04-16 14:21:11 +0700

    update

--- src/main/java/com/example/migrasi/AI/AiBaseKnowledgeService.java
+++ src/main/java/com/example/migrasi/AI/AiBaseKnowledgeService.java
@@ -379,14 +379,7 @@ public class AiBaseKnowledgeService {
                 ResponseEntity<Map> response =
                         restTemplate.postForEntity(url, request, Map.class);
 
-//                Map body = response.getBody();
-//
-//                Map message = (Map) body.get("message");
-//
-//                String content = extractJson(message.get("content").toString());
-
                 try {
-//                    JsonNode node = mapper.readTree(content);
 
                     String content = extractJson(Objects.requireNonNull(response.getBody()).get("response").toString());
 
@@ -511,19 +504,11 @@ public class AiBaseKnowledgeService {
     private LinkedHashMap<String, Object> getStringObjectLinkedHashMap(String prompt, String systemPrompt) {
         LinkedHashMap<String, Object> request = new LinkedHashMap<>();
 
-        List<Map<String, Object>> messages = new ArrayList<>();
-
-//                messages.add(Map.of("role", "system", "content", systemPrompt));
-//
-//                messages.add(Map.of("role", "user", "content", prompt));
-
         String finalPrompt =
                 "System:\n" + systemPrompt + "\n\n" +
                         "User:\n" + prompt;
 
         request.put("model", aiModel);
-
-//                request.put("messages", messages);
 
         request.put("prompt",finalPrompt);
 
